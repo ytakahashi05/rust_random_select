914c994dc37412de469fbf23330c22a2602b97f5
ytakahashi05 <blackthunder05@gmail.com>
2021-03-07 17:09:35 +0900

    change: make main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use rand::{Rng};
+
 fn main() {
-    println!("Hello, world!");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    let splited_person = guess.split(' ').collect::<Vec<&str>>();
+    let random_num = rand::thread_rng().gen_range(0..(splited_person.len()));
+    // nambuだったらprintlnする
+    let mut index = 0;
+    let is_nambu = loop {
+        if splited_person[index] == "nambu" {
+            break true
+        } else {
+            index += 1;
+            if index > splited_person.len() - 1 {
+                break false
+            }
+        }
+    };
+    if is_nambu == true {
+        println!("nambu")
+    } else {
+        println!("{}", splited_person[random_num])
+    }
+    
+    // println!("{:?}", splited_person)
 }
